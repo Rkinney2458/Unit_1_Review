58160e7c2461e2e2d8e28b6b66896d539016811e
Ryan <rkinney220@west-mec.org>
2020-01-10 16:08:38 -0700

    The code

--- tny_timer.js
+++ tny_timer.js
@@ -3,15 +3,27 @@
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Review Assignment
-
    Event Timer
-   Author: 
-   Date:   
-
+   Author: Ryan Kinney
+   Date:   1/10/20
 */
-
+showClock();
+setInterval("showClock()", 1000);
 function showClock(){
-   var thisDay = new Date(May, 19, 2018);
+   var thisDay = new Date();
+   var localDate = thisDay.toLocaleDateString();
+   var localTime = thisDay.toLocaleTimeString();
+   document.getElementById("currentTime").innerHTML = "<span>" + localDate + "</span><span>" + localTime + "</span>";
+   var j4Date = nextJuly4(thisDay);
+   j4Date.setHours(21);
+   var days = (j4Date - thisDay)/(1000*60*60*24);
+   var hrs = (days - Math.floor(days))*24;
+   var mins = (hrs - Math.floor(hrs))*60;
+   var secs = (mins - Math.floor(mins))*60;
+   document.getElementById("dLeft").textContent = Math.floor(days);
+   document.getElementById("hLeft").textContent = Math.floor(hrs);
+   document.getElementById("mLeft").textContent = Math.floor(mins);
+   document.getElementById("sLeft").textContent = Math.ceil(secs);
 }
 function nextJuly4(currentDate) {
    var cYear = currentDate.getFullYear();
